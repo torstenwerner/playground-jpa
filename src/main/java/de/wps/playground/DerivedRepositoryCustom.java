f2f203c7a262c5d8022099395ff1e679149373f8
Torsten Werner <torsten.werner@westernacher.com>
2014-03-25 10:33:37 +0100

    add Javadoc

--- src/main/java/de/wps/playground/DerivedRepositoryCustom.java
+++ src/main/java/de/wps/playground/DerivedRepositoryCustom.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.io.Serializable;
 
+/**
+ * idea from http://docs.spring.io/spring-data/data-commons/docs/1.6.1.RELEASE/reference/html/repositories.html
+ */
 public interface DerivedRepositoryCustom {
     public void saveAndRefresh(DerivedEntity entity);
 }
