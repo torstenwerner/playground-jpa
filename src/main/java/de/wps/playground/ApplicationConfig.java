3cdac81b5df47b4238cb44201f1a83421b51e405
Torsten Werner <torsten.werner@westernacher.com>
2016-09-10 20:10:47 +0200

    let spring boot create EntityManagerFactory

--- src/main/java/de/wps/playground/ApplicationConfig.java
+++ src/main/java/de/wps/playground/ApplicationConfig.java
@@ -2,16 +2,14 @@ package de.wps.playground;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
-import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "de.wps.playground")
 public class ApplicationConfig {
     @Bean
     public DataSource dataSource() {
@@ -21,16 +19,5 @@ public class ApplicationConfig {
                 .setType(EmbeddedDatabaseType.HSQL)
                 .addScript("/db/migration/V1__initial.sql")
                 .build();
-    }
-
-    @Bean
-    public EntityManagerFactory entityManagerFactory(JpaVendorAdapter jpaVendorAdapter) {
-        final LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-        factory.setJpaVendorAdapter(jpaVendorAdapter);
-        factory.setPackagesToScan("de.wps.playground");
-        factory.setDataSource(dataSource());
-        factory.afterPropertiesSet();
-
-        return factory.getObject();
     }
 }
