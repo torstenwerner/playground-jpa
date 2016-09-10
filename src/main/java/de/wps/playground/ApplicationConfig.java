6fbe2b015ed625d672f16e435b6c039afa66a766
Torsten Werner <torsten.werner@westernacher.com>
2016-09-10 20:21:38 +0200

    use flyway again in the spring boot way + downgrade hsql due to a bug

--- src/main/java/de/wps/playground/ApplicationConfig.java
+++ src/main/java/de/wps/playground/ApplicationConfig.java
@@ -1,23 +1,9 @@
 package de.wps.playground;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "de.wps.playground")
 public class ApplicationConfig {
-    @Bean
-    public DataSource dataSource() {
-        final EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-
-        return builder
-                .setType(EmbeddedDatabaseType.HSQL)
-                .addScript("/db/migration/V1__initial.sql")
-                .build();
-    }
 }
